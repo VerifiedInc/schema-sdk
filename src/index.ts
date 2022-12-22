8ca20108891ef4f19b2e6d17bf82cb9b48fbfefb
Ray Smets <rayjsmets@gmail.com>
2022-12-22 13:53:04 -0800

    [TypeBox] validator removed in favor of ajv more flexible schema validation.

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import { emailCredentialSchema } from './schemas';
 import { EmailCredentialSchemaType } from './types';
 // import types from './types';
 import { UnumSchemaObject, validateSchema } from './validate';
-import { validatorFactory } from './validatorFactory';
 
 export {
   // types
   UnumSchemaObject,
   // validator factory
-  validatorFactory,
   validateSchema,
 
   // credential schemas
