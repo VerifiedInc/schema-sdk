3005e0ed929f95111e54bdf218109d51dc9d58bb
Ray Smets <rayjsmets@gmail.com>
2023-08-30 09:57:26 -0700

    feat: new dateUsFormat (#70)
    ## Summary
    New date for use in new translation layer. More alt formats to come.
    
    ## Changes
    - new date format
    
    ## Testing
    - unit
    
    ## Checklist
    
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    _to a borderline excessive amount_
    - [x] I have made any relevant corresponding changes to the
    documentation including the project readme.
    - [x] I have run and tested the changes locally
    - [x] If it is a core feature, I have added an appropriate amount of
    unit tests.
    - [x] Any dependent changes have been merged and published in upstream
    projects

--- build/formats.js
+++ build/formats.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.booleanFormat = exports.confidenceLevelFormat = exports.documentTypeFormat = exports.genderFormat = exports.sexFormat = exports.usZipCodeFormat = exports.iso3166RegionCodeFormat = exports.iso3166USRegionCodeFormat = exports.iso3166Alpha2CountryCodeFormat = exports.iso3166CodeFormat = exports.addressFormat = exports.iso4217AmountRangeFormat = exports.iso4217AmountFormat = exports.iso4217Format = exports.dataUriBase64ImageFormat = exports.otpFormat = exports.unixMsEpochDayFormat = exports.digitsFormat = exports.optionalPhoneFormat = exports.phoneFormat = exports.ssnFormat = exports.optionalEmailFormat = exports.emailFormat = void 0;
+exports.booleanFormat = exports.confidenceLevelFormat = exports.documentTypeFormat = exports.genderFormat = exports.sexFormat = exports.usZipCodeFormat = exports.iso3166RegionCodeFormat = exports.iso3166USRegionCodeFormat = exports.iso3166Alpha2CountryCodeFormat = exports.iso3166CodeFormat = exports.addressFormat = exports.iso4217AmountRangeFormat = exports.iso4217AmountFormat = exports.iso4217Format = exports.dataUriBase64ImageFormat = exports.otpFormat = exports.dateUsFormat = exports.unixMsEpochDayFormat = exports.digitsFormat = exports.optionalPhoneFormat = exports.phoneFormat = exports.ssnFormat = exports.optionalEmailFormat = exports.emailFormat = void 0;
 // iso4317 codes: https://www.iban.com/currency-codes
 const validISO4217Codes = new Set([
     'AED',
@@ -582,7 +582,32 @@ exports.unixMsEpochDayFormat = {
         // Convert Unix timestamp (in milliseconds) to a Date object.
         const date = new Date(timestamp);
         // Ensure is 12:00:00:000
-        return date.getUTCHours() === 12 && date.getUTCMinutes() === 0 && date.getUTCSeconds() === 0 && date.getUTCMilliseconds() === 0;
+        return (date.getUTCHours() === 12 &&
+            date.getUTCMinutes() === 0 &&
+            date.getUTCSeconds() === 0 &&
+            date.getUTCMilliseconds() === 0);
+    }
+};
+/**
+ * Format to determine if the string is a valid date in the format MM-DD-YYYY, customary US format.
+ */
+exports.dateUsFormat = {
+    type: 'string',
+    validate: (input) => {
+        // Check if dateStr matches the MM-DD-YYYY format
+        if (!/^\d{2}-\d{2}-\d{4}$/.test(input)) {
+            return false;
+        }
+        // Extract date parts
+        const [month, day, year] = input.split('-').map(Number);
+        // Basic validation
+        if (month < 1 || month > 12)
+            return false;
+        if (day < 1 || day > 31)
+            return false;
+        // Accurate validation taking into account leap years and each month's max day
+        const date = new Date(year, month - 1, day);
+        return date && date.getMonth() === month - 1;
     }
 };
 /**
